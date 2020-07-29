e0209d6ccb31169ad576e63dd1995eb6f97c9eee
Jéssica Bohn <jessicabohn74@gmail.com>
2020-07-29 19:34:24 -0300

    Create ola.php
    Ola Mundo

---
+++ olamundo/ola.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title> Primeiro Exemplo PHP</title>
+    <style> 
+        h2{
+            color:#F781D8;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style> 
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+     echo "<h2>Olá,  </br>Mundo ! </h2>";
+    ?>
+
+</body>
+</html>
