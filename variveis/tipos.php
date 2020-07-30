167ae3a2b2e5fefe673b252b1e83daefe62fa693
Jéssica Bohn <jessicabohn74@gmail.com>
2020-07-29 22:43:31 -0300

    tipos de variaveis
    tipos de variaveis

---
+++ variveis/tipos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <link rel =  "stylesheet" href="_css/estilo.css"/>
+    <title> </title>
+ 
+</head>
+<body>
+    <div>
+    <?php
+      $n = 4;
+      $nome= (string)"André";
+      $idade = (int)"26André";
+
+
+     echo $nome." tem " .$idade ." anos </br>";
+ //ou
+     echo "$nome tem $idade anos </br> ";
+    ?>
+    </div>
+</body>
+</html>
